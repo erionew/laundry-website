dae5cfff4e1c77d98942a4691484586ae1eda324
Erione White <erionewhite1999@gmail.com>
2023-04-03 00:03:37 -0500

    updated styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import '../Stylesheets/header.scss'
+import { Slide } from "react-awesome-reveal";
 
 export default function Header() {
   return (
     <header className='header--container'>
-        <h2>Welcome to</h2>
-        <h1>Sud-Stainable<br></br>Laundry Co.</h1>
-        <button className='btn--teal'>Read More</button>
-        <button className='btn--outline'>Contact Us</button>
+        <Slide triggerOnce >
+          <h1>Welcome to</h1>
+          <h1 className='text--teal'>Sud-Stainable<br></br>Laundry Co.</h1>
+          <div className='css-graphic'></div>
+          <div className='header__links'>
+            <a className='a--teal'>Read More</a>
+            <a className='a--outline'>Contact Us</a>
+          </div>
+        </Slide>
+        
     </header>
   )
 }
